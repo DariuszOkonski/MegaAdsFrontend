662d6f266be24ccb988a95413af44076737e28e7
Dariusz Okoński <darek200180@gmail.com>
2023-02-09 16:47:55 +0100

    add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import React from "react";
-import { TestInterface } from "types";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const foobar: TestInterface = {
-    x: 123,
-  };
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!!! {foobar.x}</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <h1>
+          <strong>Mega </strong> Ogłoszenia
+        </h1>
+        <button>Dodaj ogłoszenie</button>
+        <div className="search">
+          <input type="text" />
+          <button>Szukaj</button>
+        </div>
       </header>
-    </div>
+      <div className="map"></div>
+    </>
   );
 }
 
