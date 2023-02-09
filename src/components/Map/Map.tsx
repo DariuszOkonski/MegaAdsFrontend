bbab7a1be48fd570ca7e9dedd85d6b63dd61889c
Dariusz Okoński <darek200180@gmail.com>
2023-02-09 17:53:08 +0100

    add map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,6 @@
-import { MapContainer } from "react-leaflet";
-import { TileLayer } from "react-leaflet";
+import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
 import "./Map.css";
+import "../../utils/fix-map-icon";
 import "leaflet/dist/leaflet.css";
 
 export const Map = () => {
@@ -11,6 +11,12 @@ export const Map = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap"
         />
+        <Marker position={[50.2657152, 18.9945008]}>
+          <Popup>
+            <h2>IT.focus</h2>
+            <p>Firma programistyczna</p>
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
