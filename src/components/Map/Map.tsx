3f5a3057053a3a70092d6f86d54108a332a21770
Dariusz Okoński <darek200180@gmail.com>
2023-02-09 17:42:50 +0100

    add map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,17 @@
+import { MapContainer } from "react-leaflet";
+import { TileLayer } from "react-leaflet";
 import "./Map.css";
+import "leaflet/dist/leaflet.css";
 
 export const Map = () => {
-  return <div className="map"></div>;
+  return (
+    <div className="map">
+      <MapContainer center={[50.2657152, 18.9945008]} zoom={20}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap"
+        />
+      </MapContainer>
+    </div>
+  );
 };
